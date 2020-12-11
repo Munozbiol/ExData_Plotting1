e95ed07d419ca545cc71d7608959e047136755b2
Munozbiol <66446136+Munozbiol@users.noreply.github.com>
2020-12-11 10:18:15 -0600

    Add files via upload
    Code added

---
+++ Plot1.R
@@ -0,0 +1,13 @@
+#load data and select the correct days
+#the file was previously charged to my working directory and wasnt necessary to unzip it.
+
+data <- read.csv("household_power_consumption.txt", sep = ";", header = TRUE, as.is = TRUE)
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+subsetdate <- subset(data, subset=(Date == "2007-02-01" | Date == "2007-02-02"))
+
+#First plot 
+png("plot1.png", width=480, height=480)
+histogram <- hist(x=as.numeric(subsetdate$Global_active_power), col = "red", 
+                  xlab = "Global Active Power (Kilowatts)", ylab = "Frequency",
+                  main = "Global Active Power")
+dev.off()
