31b0040b33c57aa59950dc696f8ae9ec6903325f
Munozbiol <66446136+Munozbiol@users.noreply.github.com>
2020-12-11 11:09:55 -0600

    Plot4 code

---
+++ plot4.R
@@ -0,0 +1,23 @@
+#load data and select the correct days
+#the file was previously charged to my working directory and wasnt necessary to unzip it.
+
+data <- read.csv("household_power_consumption.txt", sep = ";", header = TRUE, as.is = TRUE)
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+subsetdate <- subset(data, subset=(Date == "2007-02-01" | Date == "2007-02-02"))
+
+#fourth plot
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+datetime <- as.POSIXlt(paste(subsetdate$Date, subsetdate$Time, sep=" "))
+plot(datetime, subsetdate$Global_active_power, type="n", xlab="", ylab="Global Active Power", cex.lab=0.8)
+lines(datetime, subsetdate$Global_active_power)
+plot(datetime, subsetdate$Voltage, type="n", xlab="datetime", ylab="Voltage", cex.lab=0.8)
+lines(datetime, subsetdate$Voltage)
+plot(datetime, subsetdate$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering", cex.lab=0.8)
+lines(datetime, subsetdate$Sub_metering_1, col="black")
+lines(datetime, subsetdate$Sub_metering_2, col="red")
+lines(datetime, subsetdate$Sub_metering_3, col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(5,5),col=c("black","red","blue"),bty="n", cex = 0.75)
+plot(datetime, subsetdate$Global_reactive_power, type="n", xlab="datetime", ylab="Global_reactive_power", cex.lab=0.75)
+lines(datetime, subsetdate$Global_reactive_power)
+dev.off()
